28b8c4496bbb99b70355a70c89bbf6b1b65e4b0c
Romuel Webdev <johnromuelmendoza@yahoo.com>
2022-02-04 17:15:10 +0800

    add 3d

--- components/voxel-dog-loader.js
+++ components/voxel-dog-loader.js
@@ -12,26 +12,9 @@ export const DogSpinner = () => (
   />
 )
 
-export const DogContainer = forwardRef(({ children }, ref) => (
-  <Box
-    ref={ref}
-    className="voxel-dog"
-    m="auto"
-    mt={['-20px', '-60px', '-120px']}
-    mb={['-40px', '-140px', '-200px']}
-    w={[280, 480, 640]}
-    h={[280, 480, 640]}
-    position="relative"
-  >
-    {children}
-  </Box>
-))
-
 const Loader = () => {
   return (
-    <DogContainer>
-      <DogSpinner />
-    </DogContainer>
+    <DogSpinner />
   )
 }
 
