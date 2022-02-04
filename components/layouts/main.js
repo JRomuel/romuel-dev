f29c4091fb805580de45e8732567253f8b2ad4de
Romuel Webdev <johnromuelmendoza@yahoo.com>
2022-02-04 16:53:38 +0800

    add 3d

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelDogLoader from '../voxel-dog-loader'
 
-
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+    ssr: false,
+    loading: () => <VoxelDogLoader />
+  })
+  
 
 const Main = ({ children, router }) => {
     return (
@@ -18,16 +24,16 @@ const Main = ({ children, router }) => {
           <meta name="twitter:site" content="@craftzdog" />
           <meta name="twitter:creator" content="@craftzdog" />
           <meta name="twitter:image" content="/card.png" />
-          <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+          <meta property="og:site_name" content="Jr Mendoza" />
           <meta property="og:type" content="website" />
           <meta property="og:image" content="/card.png" />
-          <title>Takuya Matsuyama - Homepage</title>
+          <title>Jr Mendoza | Home</title>
         </Head>
   
         <NavBar path={router.asPath} />
   
         <Container maxW="container.md" pt={14}>
-  
+          <LazyVoxelDog />
           {children}
   
         </Container>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,63 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Image,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  Icon,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+import Layout from '../components/layouts/article'
+
 
 const Home = () => (
-  <>
-    <p>home</p>
-  </>
+<Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mt={8}
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      >
+        Hello, I&apos;m a full-stack developer based in Manila!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            JR Mendoza
+          </Heading>
+          <p>( Developer / Web Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/romuel.jpg"
+            alt="Profile image"
+          />
+        </Box>
+      </Box>
+
+    </Container>
+  </Layout>
 )
 
 export default Home
